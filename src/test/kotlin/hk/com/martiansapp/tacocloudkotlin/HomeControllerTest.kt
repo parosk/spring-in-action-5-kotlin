ae864de7d864f31dccb09303a9a5d25cc242af08
parosk <paroskwan@gmail.com>
2021-10-14 00:57:10 +0800

    ch1 - add test case and homeController

---
+++ src/test/kotlin/hk/com/martiansapp/tacocloudkotlin/HomeControllerTest.kt
@@ -0,0 +1,31 @@
+package hk.com.martiansapp.tacocloudkotlin
+
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(HomeController::class) // <1>
+class HomeControllerTest {
+    @Autowired
+    private val mockMvc // <2>
+            : MockMvc? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun testHomePage() {
+        mockMvc!!.perform(get("/")) // <3>
+                .andExpect(status().isOk()) // <4>
+                .andExpect(view().name("home")) // <5>
+                .andExpect(content().string( // <6>
+                        containsString("Welcome to...")))
+    }
+}
